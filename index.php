5b4fc9ecc42b6487ecb536a581877b159ae40d01
Marvin Curlee <marvin.curle@gmail.com>
2018-11-19 14:43:37 -0500

    updated cpu stress loop

--- index.php
+++ index.php
@@ -3,9 +3,17 @@
 print "Hello World <br> ";
 print "version 9 <br>";
 
-for($i = 0; $i < 100000000; $i++) {
+for($i = 0; $i < 100000; $i++) {
+     $str = generateRandomString(30);
+     base64_encode($str);
      $a += $i;
 }
+
+
+function generateRandomString($length = 10) {
+    return substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
+}
+
 
 
 phpinfo();
